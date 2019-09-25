8a191db676dac4082e73ad4440912c0588382a98
Tuan Phung <tuan@phung.cz>
2019-09-25 13:02:52 +0200

    updated babel

--- src/index.jsx
+++ src/index.jsx
@@ -1,87 +1,119 @@
 import React, { useEffect, useRef, useState } from 'react';
 import lottie from 'lottie-web';
 import getAnimationData from './getAnimationData';
+const CLICK_PLAY_AND_BACKWARDS = 'CLICK_PLAY_AND_BACKWARDS';
+const HOVER_PLAY_AND_STOP = 'HOVER_PLAY_AND_STOP';
+const HOVER_PLAY_AND_BACKWARDS = 'HOVER_PLAY_AND_BACKWARDS';
+const CLICK_PLAY = 'CLICK_PLAY';
+const LOOP_PLAY = 'LOOP_PLAY';
+const CLICK_PLAY_AND_SEGMENTS = 'CLICK_PLAY_AND_SEGMENTS';
 
-export const CLICK_PLAY_AND_BACKWARDS = 'CLICK_PLAY_AND_BACKWARDS';
-export const CLICK_PLAY = 'CLICK_PLAY';
-export const HOVER_PLAY_AND_STOP = 'HOVER_PLAY_AND_STOP';
-export const CLICK_PLAY_AND_SEGMENTS = 'CLICK_PLAY_AND_SEGMENTS';
+const getEvents = ({ animation, effect }) => {
+  if (effect === CLICK_PLAY_AND_SEGMENTS) {
+    return {
+      onClick: () => {
+        animation.stop()
+        animation.play()
+      },
+    };
+  }
 
-const getEvents = ({ setEvents, animation, effect }) => {
-    if (effect === CLICK_PLAY_AND_SEGMENTS) {
-        return setEvents({
-            onClick:  () => animation.playSegments([0, 60], true),
-        })
-    }
+  if (effect === CLICK_PLAY) {
+    return {
+      onClick: () => {
+        animation.stop();
+        animation.play();
+      },
+    };
+  }
 
-    if (effect === CLICK_PLAY) {
-        return setEvents({
-            onClick: () => {
-                animation.stop();
-                animation.play();
-            }
-        })
-    }
-
-    if (effect === HOVER_PLAY_AND_STOP) {
-        return setEvents({
-            onMouseEnter: () => animation.play(),
-            onMouseLeave: () => animation.stop(),
-        })
-    }
-
-    if (effect === CLICK_PLAY_AND_BACKWARDS) {
-        let directionMenu = 1;
-
-        return setEvents({
-            onClick: () => {
-                animation.setDirection(directionMenu);
-                animation.play();
-                directionMenu = -directionMenu;
-            }
-        })
-    }
-}
-
-export default ({ options, animationKey, ariaLabel, effect, loop, autoplay, ...other }) => {
-    const element = useRef(null);
-    const [animation, setAnimation] = useState(null);
-    const [events, setEvents] = useState();
-
-    useEffect(() => {
-        let defaultOptions = {
-            container: element.current,
-            renderer: 'svg',
-            animationData: getAnimationData(animationKey),
-            loop,
-            autoplay,
-            ...options,
-        };
-
-        setAnimation(lottie.loadAnimation(defaultOptions));
-        
-        return () => {
-            animation.destroy();
-            setAnimation(null);
-        };
-    }, []);
-    
-    getEvents({ setEvents, animation, effect })
+  if (effect === HOVER_PLAY_AND_STOP) {
+    return {
+      onMouseEnter: () => animation.play(),
+      onMouseLeave: () => animation.stop(),
+    };
+  }
 
 
-    const defaultStyles = {
-        overflow: 'hidden',
-        outline: 'none',
-    }
+  if (effect === HOVER_PLAY_AND_BACKWARDS) {
+    let directionMenu = 1;
 
-    return (
-        <div
-            ref={element}
-            aria-label={ariaLabel}
-            style={defaultStyles}
-            tabIndex="0"
-            {...events}
-            {...other}
-        />
-    );
+
+    return {
+      onMouseEnter: () => {
+        animation.setDirection(1);
+        animation.play();
+
+
+      },
+      onMouseLeave: () => {
+
+        animation.setDirection(-1);
+        animation.play();
+
+        console.log('onMouseLeave')
+
+      },
+    };
+  }
+
+  if (effect === CLICK_PLAY_AND_BACKWARDS) {
+    let directionMenu = 1;
+
+    return {
+      onClick: () => {
+        animation.setDirection(directionMenu);
+        animation.play();
+        directionMenu = -directionMenu;
+      },
+    };
+  }
+};
+
+export default ({
+  options,
+  animationKey,
+  ariaLabel,
+  effect,
+  loop,
+  autoplay,
+  ...other
+}) => {
+  const element = useRef(null);
+  const [animation, setAnimation] = useState(null);
+  const [events, setEvents] = useState();
+
+  useEffect(() => {
+    let defaultOptions = {
+      container: element.current,
+      renderer: 'svg',
+      animationData: getAnimationData(animationKey),
+      loop: loop || effect === LOOP_PLAY,
+      autoplay: autoplay || effect === LOOP_PLAY,
+      ...options,
+    };
+
+    setAnimation(lottie.loadAnimation(defaultOptions));
+
+    return () => {
+      animation.destroy();
+      setAnimation(null);
+    };
+  }, []);
+
+  const defaultStyles = {
+    overflow: 'hidden',
+    outline: 'none',
+  };
+
+  return (
+    <div
+      ref={element}
+      aria-label={ariaLabel}
+      style={defaultStyles}
+      tabIndex="0"
+      {...getEvents({ animation, effect })}
+      {...other}
+    />
+  );
 };
