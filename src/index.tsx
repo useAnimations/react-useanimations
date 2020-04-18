ccb6479a818e1a21be86e9291f25c22b82abb187
Sebastian Vera <u.verainf@gmail.com>
2020-04-18 20:30:35 +0200

    refactor: transform class component to functional and use hooks

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
-import lottie, { AnimationItem, AnimationConfigWithData, AnimationConfig } from 'lottie-web';
+import React, { useState, useEffect, useRef } from 'react';
 import styled from '@emotion/styled';
-import { getEffect, getAnimationData, getEvents, LOOP_PLAY, AnimationKey } from './utils';
+import lottie, {
+  AnimationItem,
+  AnimationConfigWithData,
+  AnimationConfig,
+} from 'lottie-web';
+
+import {
+  getEffect,
+  getAnimationData,
+  getEvents,
+  LOOP_PLAY,
+  AnimationKey,
+} from './utils';
 
 const ColoredIcon = styled.div`
   path {
@@ -11,31 +22,31 @@ const ColoredIcon = styled.div`
 `;
 
 type Props = {
-  animationKey: AnimationKey,
-  options?: Partial<AnimationConfig>,
+  animationKey: AnimationKey;
+  options?: Partial<AnimationConfig>;
   size?: number;
   loop?: AnimationConfig['loop'];
   autoplay?: AnimationConfig['autoplay'];
   style?: React.CSSProperties;
 };
 
-type State = {
-  animation: AnimationItem | null,
-};
+const UseAnimations: React.FC<Props> = ({
+  animationKey,
+  size = 24,
+  loop,
+  autoplay,
+  style,
+  options,
+  ...other
+}) => {
+  const [animation, setAnimation] = useState<AnimationItem>();
+  const ref = useRef<HTMLDivElement>(null);
 
-export default class UseAnimations extends React.Component<Props, State> {
-  state: State = {
-    animation: null,
-  };
-
-  element = React.createRef<HTMLDivElement>();
-
-  componentDidMount() {
-    const { animationKey, loop, autoplay, options } = this.props;
+  useEffect(() => {
     const animEffect = getEffect(animationKey);
 
     const defaultOptions: AnimationConfigWithData = {
-      container: this.element.current as Element,
+      container: ref.current as Element,
       renderer: 'svg',
       animationData: getAnimationData(animationKey),
       loop: loop || animEffect === LOOP_PLAY,
@@ -43,46 +54,33 @@ export default class UseAnimations extends React.Component<Props, State> {
       ...options,
     };
 
-    this.setAnimation(lottie.loadAnimation(defaultOptions));
-  }
+    setAnimation(lottie.loadAnimation(defaultOptions));
+  }, []);
 
-  componentWillUnmount() {
-    const { animation } = this.state;
+  const defaultStyles = {
+    overflow: 'hidden',
+    outline: 'none',
+    width: size,
+    ...style,
+  };
 
-    if (animation) {
-      animation.destroy();
-    }
+  let animationProps = {
+    ref,
+    ...other,
+    style: defaultStyles,
+  };
 
-    this.setAnimation(null);
-  }
-
-  setAnimation = (animation: AnimationItem | null) => this.setState({ animation });
-
-  render() {
-    const { animationKey, size = 24, style, ...other } = this.props;
-    const { animation } = this.state;
-
-    if (!animation) {
-      throw new Error('animation state not set');
-    }
-
-    const defaultStyles = {
-      overflow: 'hidden',
-      outline: 'none',
-      width: size,
-      ...style,
-    };
-
-    const animationProps = {
-      ref: this.element,
-      ...other,
-      style: defaultStyles,
+  if (animation) {
+    animationProps = {
+      ...animationProps,
       ...getEvents({
         animation,
         animEffect: getEffect(animationKey),
       }),
     };
-
-    return <ColoredIcon {...animationProps} />;
   }
-}
+
+  return <ColoredIcon {...animationProps} />;
+};
+
+export default UseAnimations;
