b30328a083bbcd233c56a465737ac25fe11b0db1
Sebastian Vera <u.verainf@gmail.com>
2020-04-18 22:21:45 +0200

    build: update dist output

--- dist/index.js
+++ dist/index.js
@@ -1,51 +1,47 @@
 "use strict";
 
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports["default"] = void 0;
 
-var _react = _interopRequireDefault(require("react"));
+var _react = _interopRequireWildcard(require("react"));
+
+var _styled = _interopRequireDefault(require("@emotion/styled"));
 
 var _lottieWeb = _interopRequireDefault(require("lottie-web"));
-
-var _styledComponents = _interopRequireDefault(require("styled-components"));
 
 var _utils = require("./utils");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
 
-function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
-
-function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
 
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-function _createSuper(Derived) { return function () { var Super = _getPrototypeOf(Derived), result; if (_isNativeReflectConstruct()) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(n); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return; var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
+function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
+
+function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
 
 function _templateObject() {
   var data = _taggedTemplateLiteral(["\n  path {\n    stroke: ", ";\n    fill: ", ";\n  }\n"]);
@@ -59,110 +55,69 @@ function _templateObject() {
 
 function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
 
-var ColoredIcon = _styledComponents["default"].div(_templateObject(), function (_ref) {
+var ColoredIcon = _styled["default"].div(_templateObject(), function (_ref) {
   var strokeColor = _ref.strokeColor;
-  return strokeColor || 'currentColor;';
+  return strokeColor || 'currentColor';
 }, function (_ref2) {
   var fillColor = _ref2.fillColor;
-  return fillColor || 'currentColor;';
+  return fillColor || 'currentColor';
 });
 
-var UseAnimations = /*#__PURE__*/function (_React$Component) {
-  _inherits(UseAnimations, _React$Component);
+var UseAnimations = function UseAnimations(_ref3) {
+  var animationKey = _ref3.animationKey,
+      _ref3$size = _ref3.size,
+      size = _ref3$size === void 0 ? 24 : _ref3$size,
+      loop = _ref3.loop,
+      autoplay = _ref3.autoplay,
+      style = _ref3.style,
+      options = _ref3.options,
+      _onClick = _ref3.onClick,
+      other = _objectWithoutProperties(_ref3, ["animationKey", "size", "loop", "autoplay", "style", "options", "onClick"]);
 
-  var _super = _createSuper(UseAnimations);
+  var _useState = (0, _react.useState)(),
+      _useState2 = _slicedToArray(_useState, 2),
+      animation = _useState2[0],
+      setAnimation = _useState2[1];
 
-  function UseAnimations() {
-    var _this;
+  var ref = (0, _react.useRef)(null);
+  (0, _react.useEffect)(function () {
+    var animEffect = (0, _utils.getEffect)(animationKey);
 
-    _classCallCheck(this, UseAnimations);
+    var defaultOptions = _objectSpread({
+      container: ref.current,
+      renderer: 'svg',
+      animationData: (0, _utils.getAnimationData)(animationKey),
+      loop: loop || animEffect === _utils.LOOP_PLAY,
+      autoplay: autoplay || animEffect === _utils.LOOP_PLAY
+    }, options);
 
-    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
-      args[_key] = arguments[_key];
+    setAnimation(_lottieWeb["default"].loadAnimation(defaultOptions));
+  }, []);
+
+  var defaultStyles = _objectSpread({
+    overflow: 'hidden',
+    outline: 'none',
+    width: size
+  }, style);
+
+  var eventProps = animation ? (0, _utils.getEvents)({
+    animation: animation,
+    animEffect: (0, _utils.getEffect)(animationKey)
+  }) : undefined;
+
+  var animationProps = _objectSpread({
+    ref: ref
+  }, other, {
+    style: defaultStyles
+  }, eventProps, {
+    onClick: function onClick(e) {
+      if (_onClick) _onClick(e);
+      if (eventProps && 'onClick' in eventProps) eventProps.onClick();
     }
+  });
 
-    _this = _super.call.apply(_super, [this].concat(args));
-
-    _defineProperty(_assertThisInitialized(_this), "state", {
-      animation: null
-    });
-
-    _defineProperty(_assertThisInitialized(_this), "element", _react["default"].createRef());
-
-    _defineProperty(_assertThisInitialized(_this), "setAnimation", function (animation) {
-      return _this.setState({
-        animation: animation
-      });
-    });
-
-    return _this;
-  }
-
-  _createClass(UseAnimations, [{
-    key: "componentDidMount",
-    value: function componentDidMount() {
-      var _this$props = this.props,
-          animationKey = _this$props.animationKey,
-          loop = _this$props.loop,
-          autoplay = _this$props.autoplay,
-          options = _this$props.options;
-      var animEffect = (0, _utils.getEffect)(animationKey);
-
-      var defaultOptions = _objectSpread({
-        container: this.element.current,
-        renderer: 'svg',
-        animationData: (0, _utils.getAnimationData)(animationKey),
-        loop: loop || animEffect === _utils.LOOP_PLAY,
-        autoplay: autoplay || animEffect === _utils.LOOP_PLAY
-      }, options);
-
-      this.setAnimation(_lottieWeb["default"].loadAnimation(defaultOptions));
-    }
-  }, {
-    key: "componentWillUnmount",
-    value: function componentWillUnmount() {
-      var animation = this.state.animation;
-
-      if (animation) {
-        animation.destroy();
-      }
-
-      this.setAnimation(null);
-    }
-  }, {
-    key: "render",
-    value: function render() {
-      var _this$props2 = this.props,
-          animationKey = _this$props2.animationKey,
-          size = _this$props2.size,
-          style = _this$props2.style,
-          other = _objectWithoutProperties(_this$props2, ["animationKey", "size", "style"]);
-
-      var animation = this.state.animation;
-
-      var defaultStyles = _objectSpread({
-        overflow: 'hidden',
-        outline: 'none',
-        width: size
-      }, style);
-
-      var animationProps = _objectSpread({
-        ref: this.element
-      }, other, {
-        style: defaultStyles
-      }, (0, _utils.getEvents)({
-        animation: animation,
-        animEffect: (0, _utils.getEffect)(animationKey)
-      }));
-
-      return /*#__PURE__*/_react["default"].createElement(ColoredIcon, animationProps);
-    }
-  }]);
-
-  return UseAnimations;
-}(_react["default"].Component);
-
-exports["default"] = UseAnimations;
-UseAnimations.defaultProps = {
-  size: 24
+  return /*#__PURE__*/_react["default"].createElement(ColoredIcon, animationProps);
 };
+
+var _default = UseAnimations;
+exports["default"] = _default;
