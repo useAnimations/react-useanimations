67ff5faae5176a5f2b07f37b8800fe2d0336e19c
Marek Feikus <mfeikus@gmail.com>
2020-06-26 13:03:54 +0700

    final version of addAnimationFiles script => output to typescript

--- src/bin/addAnimationFiles.sh
+++ src/bin/addAnimationFiles.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
+set -ex
 
-### WHEN NEW ANIMATIONS ARE ADDED TO THE /lib, CHANGE YOUR PWD TO /lib & RUN THIS SCRIPT TO AUTOMATE THE REST
+### WHEN NEW ANIMATIONS ARE ADDED TO THE /lib, 
+### CHANGE YOUR PWD TO /lib (very important step) & RUN THIS SCRIPT TO AUTOMATE THE REST
+touch "../temp.ts"
+echo "export const ANIMATION_KEYS = {" >> "../temp.ts"
+
 for file in *; do
   if [[ -f "$file" ]]; then
+    echo "  ${file%.*}: '${file%.*}'," >> "../temp.ts"
     mkdir "${file%.*}"
     mv "$file" "${file%.*}"
-    touch "index.js"
-    echo "import ${file%.*} from './$file';" >> "index.js"
-    echo "" >> "index.js"
-    echo "export default { animationData: ${file%.*}, animationKey: '${file%.*}' };" >> "index.js"
-    mv "index.js" "${file%.*}"
+    touch "index.ts"
+    echo "import { ANIMATION_KEYS } from '../../utils/constants';" >> "index.ts"
+    echo "import ${file%.*} from './$file';" >> "index.ts"
+    echo "" >> "index.ts"
+    echo "export default { animationData: ${file%.*}, animationKey: ANIMATION_KEYS.${file%.*} };" >> "index.ts"
+    mv "index.ts" "${file%.*}"
   fi
 done
+
+echo "};" >> "../temp.ts"
+cat "../temp.ts" >> "../utils/constants.ts"
+rm "../temp.ts"
